08b99ed92f4ac091b667befea0a8aadc3fd149be
xinmingyao <yaoxinming@gmail.com>
2014-06-20 18:16:32 +0800

    add srtp

--- src/lua_srtp.c
+++ src/lua_srtp.c
@@ -2,13 +2,19 @@
 #include "lua.h"
 #include "lauxlib.h"
 #include "assert.h"
-
+#include "crypto_math.h"
 #include <string.h>
-#define false -1
-#define true 0
+#define false 0
+#define true 1
 static int
 configure_srtp_session(srtp_t *session, uint8_t * key,enum transmission_type type);
 
+static int
+lsrtp_init(lua_State *L){
+  int res = srtp_init();
+  printf("init srtp:%d\n",res);
+  return 0;
+}
 static int
 lnew(lua_State *L) {
   struct lua_srtp * lsrtp = lua_newuserdata(L, sizeof(*lsrtp));
@@ -31,8 +37,16 @@ ldestroy(lua_State *L){
 static int
 lset_rtp(lua_State *L){
   struct lua_srtp  * srtp = lua_touserdata(L,1);
-  char * send_key = luaL_checkstring(L,2);
-  char * receiving_key = luaL_checkstring(L,3);
+  char * send_key = lua_touserdata(L,2);
+  char * receiving_key = lua_touserdata(L,3);
+
+
+  printf("set master key/salt to %s/", octet_string_hex_string(send_key, 16));
+  printf("%s\n", octet_string_hex_string(send_key+16, 14));
+
+  printf("set master key/salt to %s/", octet_string_hex_string(receiving_key, 16));
+  printf("%s\n", octet_string_hex_string(receiving_key+16, 14));
+
   if (configure_srtp_session(&srtp->send_session, send_key, SENDING)
       && configure_srtp_session(&srtp->receive_session, receiving_key, RECEIVING)){
     srtp->active = true;
@@ -59,10 +73,13 @@ lprotect_rtp(lua_State *L){
   int res = srtp_protect(srtp->send_session,buffer,&len);
   if(res == 0){
     lua_pushboolean(L,true);
+    lua_pushinteger(L,len);
+    return 2;
   }else{
+    printf ("protected rtp error:%d\n",res);
     lua_pushboolean(L,false);
+    return 1;
   }
-  return 1;
 }
 
 
@@ -80,10 +97,13 @@ lunprotect_rtp(lua_State *L){
   int res = srtp_unprotect(srtp->send_session,buffer,&len);
   if(res == 0){
     lua_pushboolean(L,true);
+    lua_pushinteger(L,len);
+    return 2;
   }else{
+    printf ("unprotected rtp error:%d\n",res);
     lua_pushboolean(L,false);
+    return 1;
   }
-  return 1;
 }
 
 
@@ -95,7 +115,7 @@ lunprotect_rtcp(lua_State *L){
 
 static int lunpack_rtp(lua_State *L){
   rtp_msg_t * message = lua_touserdata(L,1);
-  int len = luaL_checkinteger(L,2) - 12;
+  int len = luaL_checkinteger(L,2) - sizeof(srtp_hdr_t);
   char * msg = malloc(len);
   memcpy(msg,message->body,len);
   lua_pushlightuserdata(L,msg);
@@ -128,7 +148,7 @@ static int lpack_rtp(lua_State *L){//msg,sz,ssrc,ts,seq|str,ssrc,ts,seq
   message->header.ts      = htonl(ts);
   message->header.seq     = htonl(seq);
   message->header.m       = 0;
-  message->header.pt      = 0x1;
+  message->header.pt      = 0x100;
   message->header.version = 2;
   message->header.p       = 0;
   message->header.x       = 0;
@@ -139,7 +159,7 @@ static int lpack_rtp(lua_State *L){//msg,sz,ssrc,ts,seq|str,ssrc,ts,seq
     free(data);
   }
   lua_pushlightuserdata(L, message);
-  lua_pushinteger(L, (int)len);  
+  lua_pushinteger(L, (int)(sizeof(srtp_hdr_t)+len));  
   return 2;
 }
 
@@ -160,6 +180,7 @@ luaopen_lua_srtp(lua_State *L) {
     { "unprotect_rtcp",lunprotect_rtcp},
     { "pack_rtp",lpack_rtp},
     { "unpack_rtp",lunpack_rtp},
+    { "srtp_init",lsrtp_init},
     { NULL, NULL },
   };
   luaL_newlib(L,l);
@@ -186,8 +207,7 @@ configure_srtp_session(srtp_t *session, uint8_t * key,
   policy.key = key;
   int res = srtp_create(session, &policy);
   if(res!=0){
-    printf("create srtp error!\n");
+    printf("create srtp error:%d!\n",res);
   }
-  return res;
-  //return res!=0? false:true;
+  return res!=0? false:true;
 }
